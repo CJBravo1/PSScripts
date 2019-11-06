2ba50c477154ebd9d981e75042fa3d80d20a024c
cjorenby-cncy <cjorenby@concurrency.com>
2019-11-06 14:10:27 -0600

    fsd

--- syncgit.bash
+++ syncgit.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 git pull --all
-
 echo "Enter your Commit Message"
-echo "If no Changes have been made by you, Press ENTER"
+echo "If no Changes have been made, Press Enter"
 read "cMessage"
 
 git add *
 git commit -a -m "$cMessage"
 
 git push origin master
+
+
