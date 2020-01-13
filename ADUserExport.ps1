13a645089c993daa33bdebbc55f304320ebf1c85
Chris Jorenby <cjorenbyisnow@gmail.com>
2020-01-13 15:04:32 -0600

    Cleanup

--- ADUserExport.ps1
+++
@@ -1,7 +0,0 @@
-﻿Import-Module ActiveDirectory
-Write-Host "Gathering Users" -ForegroundColor Green
-
-Get-ADUser -Filter * -Properties * | select mail,mailnickname,proxyAddresses,msExchMailboxGuid,msExchArchiveGuid,legacyExchangeDN,msExchRecipientDisplayType,msExchRecipientTypeDetails | Export-Clixml C:\Temp\adusers.xml
-Get-ADUser -Filter * -Properties * | select mail,mailnickname,proxyAddresses,msExchMailboxGuid,msExchArchiveGuid,legacyExchangeDN,msExchRecipientDisplayType,msExchRecipientTypeDetails | Export-Csv -NoTypeInformation C:\Temp\adusers.csv
-
-Write-Host "Exports located in C:\temp" -ForegroundColor Green
