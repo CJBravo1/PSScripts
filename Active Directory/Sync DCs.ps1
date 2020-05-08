fe20ce210606a7540185cd381d066d74c92851f7
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2020-05-08 15:12:14 -0500

    Update Sync DCs.ps1
    Remove Server name

--- Active Directory/Sync DCs.ps1
+++ Active Directory/Sync DCs.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host "Force Domain Controller Replication, and then sysc BosCorpAADC to Office365" -ForegroundColor Green
+﻿Write-Host "Force Domain Controller Replication" -ForegroundColor Green
 #Creates variable to grab the server name of the closest Domain Controller to run the DC replication from.
 $DC = $env:LOGONSERVER -replace ‘\\’,""
 
