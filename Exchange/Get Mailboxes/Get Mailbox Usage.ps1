23fbe625a1614bc4bee04b9f940f1b52fdc346e8
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2020-05-08 09:28:52 -0500

    New Script

---
+++ Exchange/Get Mailboxes/Get Mailbox Usage.ps1
@@ -0,0 +1,39 @@
+Write-Host "Use this script to get email messages sent to distribution groups over the past 30 Days." -ForegroundColor Green
+
+#Connect to Office 365
+$UserCredential = Get-Credential -Message "Enter your Office 365 Credentials"
+
+#Make new session
+Write-Host "Connecting to Office 365" -ForegroundColor Yellow
+$Session = Get-PSSession | where {$_.ConfigurationName -eq "Microsoft.Exchange"}
+
+if ($session -eq $null)
+{
+    Connect-ExchangeOnline
+}
+
+
+#Check and Remove Output files
+$outputCSV = Test-Path .\output.csv
+$uniqueAddressesCSV = Test-Path .\UniqueAddresses.csv
+
+if ($outputCSV -eq $true) {rm .\output.csv}
+if ($uniqueAddressesCSV -eq $true) {rm .\UniqueAddresses.csv}
+
+#Get End Date and Start Date
+$dateEnd = get-date
+$dateStart = $dateEnd.AddDays(-30)
+
+#Get Groups and Message trace
+$Mailboxes = Get-Mailbox
+$Mailboxes | ForEach-Object {Get-MessageTrace -RecipientAddress $_.primarysmtpaddress -startDate $dateStart -EndDate $dateEnd | Export-Csv -NoTypeInformation .\output.csv}
+
+
+#Show Unique addresses
+$data = Import-Csv .\output.csv
+$data = $data | select recipientaddress -Unique 
+$data | Export-Csv .\UniqueAddresses.csv
+
+#Launch Excel
+Invoke-Command .\output.csv
+Invoke-Command .\UniqueAddresses.csv
