2c801ca87296cabfc0cbf8f39a9c415e59511375
Chris Jorenby <cjorenby@cyberadvisors.com>
2022-01-18 14:35:28 -0600

    Merge Active Directory Folders

--- Active Directory/ConnectToActiveDirectory.ps1
+++ Active Directory/ConnectToActiveDirectory.ps1
@@ -2,6 +2,6 @@ Write-Host "Connecting to Active Directory" -ForegroundColor Green
 $ADServer = $env:LOGONSERVER -replace "\\",""
 Write-Host "AD Server: $ADServer" -ForegroundColor Yellow
 $CurrentUser = $env:USERNAME
-$adminCreds = Get-Credential DOMAIN\$CurrentUser"
+$adminCreds = Get-Credential "DOMAIN\$CurrentUser"
 $ActiveDirectory = New-PSSession -ComputerName $ADServer -Credential $adminCreds
 Import-Module ActiveDirectory -PSSession $ActiveDirectory
--- Active Directory/Sync DCs.ps1
+++ Active Directory/Sync DCs.ps1
@@ -1,7 +1,11 @@
 ﻿Write-Host "Force Domain Controller Replication" -ForegroundColor Green
-#Creates variable to grab the server name of the closest Domain Controller to run the DC replication from.
-$DC = $env:LOGONSERVER -replace ‘\\’,""
-
-#Replication Command
-repadmin /syncall $DC /APed
-
+#Get Credentials
+$CurrentUser = $env:USERNAME
+if ($null -eq $adminCreds)
+{
+    $adminCreds = Get-Credential "DOMAIN\$CurrentUser"
+}
+#Invoke AD Sync Command
+$ADServer = $env:LOGONSERVER -replace ‘\\’,""
+Write-Host "Syncing Active Directory On Server $ADServer" -ForegroundColor Green
+Invoke-Command -ComputerName $ADServer -Credential $adminCreds -ScriptBlock {repadmin /syncall /APed} | out-null
