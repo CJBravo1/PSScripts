f0cc2e789d1c92d97ef0898e1357cc6e05afa0d5
Chris Jorenby <cjorenby@cyberadvisors.com>
2022-07-18 13:47:30 -0500

    New Teams Export Script

---
+++ Teams/Teams Export/ExportTeams.ps1
@@ -0,0 +1,32 @@
+#Check for Admin Credentials
+if ($null -eq $adminCreds)
+{
+    $adminCreds = Get-Credential -Message "Enter Teams Credentials"
+    Connect-MicrosoftTeams -Credential $adminCreds
+}
+
+#Is Teams Connected?
+$TeamsTenant = get-cstenant
+
+#if ($null -eq $TeamsTenant)
+#{
+#    Connect-MicrosoftTeams -Credential $adminCreds
+#}
+
+#Create Export Directory
+mkdir $TeamsTenant.DisplayName
+Set-Location $TeamsTenant.DisplayName
+mkdir "Team Members"
+
+#Export Teams
+Write-Host "Gathering and Exporting Teams and Members" -ForegroundColor Green
+$Teams = Get-Team
+$Teams | Export-Csv -NoTypeInformation "allTeams.csv"
+
+#Export Team Members
+$teams | ForEach-Object {
+        $TeamName = $_.Displayname
+        $TeamMailName = $_.MailNickName
+        Write-Host $TeamName -ForegroundColor Cyan
+        Get-TeamUser -GroupId $_.GroupID | Export-Csv -NoTypeInformation ".\Team Members\$TeamMailName.csv"
+    }
