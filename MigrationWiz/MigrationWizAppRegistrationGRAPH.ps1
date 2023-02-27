c30440b1e26efb453ab4f91fc211b82d6538506a
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-02-27 17:38:16 -0600

    MigrationWiz Azure App Registration with Graph PSModule - UNDER CONSTRUCTION!

--- MigrationWiz/MigrationWizAppRegistrationGRAPH.ps1
+++ MigrationWiz/MigrationWizAppRegistrationGRAPH.ps1
@@ -16,19 +16,29 @@ Write-Host "Connected As $mgContextuser" -ForegroundColor Yellow
 
 # Define the application registration details
 $AppName = "migrationWiz"
-$RedirectUri = "urn:ietf:wg:oauth:2.0:oob"
+#$RedirectUri = 'urn:ietf:wg:oauth:2.0:oob'
 $ResourceAccess = @(
-    [Microsoft.Graph.RequiredResourceAccess]@{
-        Id = "3b5f3d61-589b-4a3c-a359-5dd4b5ee5bd5"
-        Type = "Scope"
+        @{
+        ResourceAppId = "00000002-0000-0ff1-ce00-000000000000"
+        ResourceAccess = @(
+            @{
+                Id = "3b5f3d61-589b-4a3c-a359-5dd4b5ee5bd5"
+                Type = "Scope"
+            }
+        )
     },
-    [Microsoft.Graph.RequiredResourceAccess]@{
-        Id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
-        Type = "Scope"
+    @{
+        ResourceAppId = "00000003-0000-0000-c000-000000000000"
+        ResourceAccess = @(
+            @{
+                Id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+                Type = "Scope"
+            }
+        )
     }
 )
-
 # Register the application
+#$AppRegistration = New-MgApplication -DisplayName $AppName -DefaultRedirectUri $RedirectUri -RequiredResourceAccess $ResourceAccess
 $AppRegistration = New-MgApplication -DisplayName $AppName -DefaultRedirectUri $RedirectUri -RequiredResourceAccess $ResourceAccess
 
 # Export the client ID and tenant ID
@@ -36,5 +46,5 @@ $ClientId = $AppRegistration.AppId
 $TenantId = (Get-MgOrganization).Id
 
 # Output the client ID and tenant ID
-Write-Host "Client ID: $ClientId"
-Write-Host "Tenant ID: $TenantId"
+Write-Host "Client ID: $ClientId" -ForegroundColor Green
+Write-Host "Tenant ID: $TenantId" -ForegroundColor Green
