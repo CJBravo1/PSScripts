e755bf4fc3465641f10b12746ecdade7d12103ea
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-03-08 12:27:28 -0600

    AdminCreds Check and WindowTitle Modification

--- Exchange/ConnectToOnPremExchange.ps1
+++ Exchange/ConnectToOnPremExchange.ps1
@@ -1,15 +1,18 @@
-﻿#Change Window Title
-$host.ui.RawUI.WindowTitle = "Exchange Onpremises"
-$PSSession = Get-PSSession | where {$_.configurationName -like "*exchange"}
+﻿#Check for existing PS Session
+$PSSession = Get-PSSession | Where-Object {$_.configurationName -like "*exchange"}
 
-if ($adminCreds -eq $null)
+#Check for Admin Creds
+if ($null -eq $adminCreds)
 {
     $adminCreds = Get-Credential -Message "Enter your Admin Credentials"
 }
 
-if ($PSSession -eq $null)
+if ($null -eq $PSSession)
 {
     $ExchServer = Read-Host "Enter Exchange Server"
     $ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$ExchServer/powershell" -Credential $adminCreds
     Import-PSSession $ExchSession
+    $acceptedDomain = Get-AcceptedDomain | where {$_.Default -eq $true}
+    $host.ui.rawui.windowtitle="$acceptedDomain Onprem Exchange"
+    Write-Host "Have a lot of fun..." -ForegroundColor Green
 }
