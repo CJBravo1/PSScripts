8c335186e3765d11be6a7d3ab7c8ae5bd88ed299
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-03-08 12:21:41 -0600

    Add check for $adminCreds

--- Exchange/ConnectToOnPremExchange.ps1
+++ Exchange/ConnectToOnPremExchange.ps1
@@ -2,9 +2,14 @@
 $host.ui.RawUI.WindowTitle = "Exchange Onpremises"
 $PSSession = Get-PSSession | where {$_.configurationName -like "*exchange"}
 
+if ($adminCreds -eq $null)
+{
+    $adminCreds = Get-Credential -Message "Enter your Admin Credentials"
+}
+
 if ($PSSession -eq $null)
 {
     $ExchServer = Read-Host "Enter Exchange Server"
-    $ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$ExchServer/powershell"
+    $ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$ExchServer/powershell" -Credential $adminCreds
     Import-PSSession $ExchSession
 }
