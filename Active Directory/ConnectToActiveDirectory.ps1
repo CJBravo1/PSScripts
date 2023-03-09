04bb5267f469ccf62e5580a34d056518ec9099ce
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-03-09 09:41:22 -0600

    Old Script Cleanup

--- Active Directory/ConnectToActiveDirectory.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Connecting to Active Directory" -ForegroundColor Green
-$ADServer = $env:LOGONSERVER -replace "\\",""
-Write-Host "AD Server: $ADServer" -ForegroundColor Yellow
-$CurrentUser = $env:USERNAME
-$adminCreds = Get-Credential "DOMAIN\$CurrentUser"
-$ActiveDirectory = New-PSSession -ComputerName $ADServer -Credential $adminCreds
-Import-Module ActiveDirectory -PSSession $ActiveDirectory
