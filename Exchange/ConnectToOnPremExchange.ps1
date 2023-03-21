dcf0465dd797cfa07fcb31d75a0bdd91fddd8176
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-03-21 11:42:11 -0500

    Where to Where-Object

--- Exchange/ConnectToOnPremExchange.ps1
+++ Exchange/ConnectToOnPremExchange.ps1
@@ -12,7 +12,7 @@ if ($null -eq $PSSession)
     $ExchServer = Read-Host "Enter Exchange Server"
     $ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$ExchServer/powershell" -Credential $adminCreds
     Import-PSSession $ExchSession
-    $acceptedDomain = Get-AcceptedDomain | where {$_.Default -eq $true}
+    $acceptedDomain = Get-AcceptedDomain | Where-Object {$_.Default -eq $true}
     $host.ui.rawui.windowtitle="$acceptedDomain Onprem Exchange"
     Write-Host "Have a lot of fun..." -ForegroundColor Green
 }
