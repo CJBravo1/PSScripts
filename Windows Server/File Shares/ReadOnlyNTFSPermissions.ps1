7b758da829b3440c43c0599681d5453a7f9da32e
Chris Jorenby <25773132+CJBravo1@users.noreply.github.com>
2023-05-12 14:59:33 -0500

    New NTFS ReadOnly script

---
+++ Windows Server/File Shares/ReadOnlyNTFSPermissions.ps1
@@ -0,0 +1,22 @@
+#Use this script to set Directory NTFS permisions for users to Read and Execute
+
+$HomeDirectories = Get-ChildItem "C:\Temp\Directory"
+
+foreach ($Directory in $HomeDirectories)
+{
+    Write-Host $Directory.Name -ForegroundColor Cyan
+    #Get ACL
+    $HomeDirectoryACL = Get-Acl $Directory.FullName
+
+    #Filter To Specific User
+    $ADUser = $HomeDirectoryACL.Access | Where-Object {$_.IdentityReference -notlike "S-1*" -and $_.IdentityReference -notlike "BUILTIN\*" -and $_.IdentityReference -notlike "NT AUTHORITY\*"}
+
+    #Create New ACL Rule
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($ADUser.IdentityReference,"ReadAndExecute", "ContainerInherit,ObjectInherit", "none", "allow")
+
+    #Add ACL Rule to Variable
+    $HomeDirectoryACL.SetAccessRule($rule)
+
+    #Set ACL Rule
+    Set-Acl -Path $Directory.FullName -AclObject $HomeDirectoryACL
+}
