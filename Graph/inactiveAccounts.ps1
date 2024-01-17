dce062272e528ba9c7cc30687b4453ee44c908e3
Chris Jorenby <156940640+cjorenbyArcticWolf@users.noreply.github.com>
2024-01-17 15:22:09 -0600

    New Inactive Account Audit Script

---
+++ Graph/inactiveAccounts.ps1
@@ -0,0 +1,32 @@
+#Connect to Graph
+if ($null -eq (Get-MGContext))
+{
+    Connect-MgGraph -Scopes 'User.Read.All', 'Directory.AccessAsUser.All', 'User.ReadBasic.All', 'User.ReadWrite.All', 'Directory.Read.All', 'Directory.ReadWrite.All', 'Group.Read.All', 'User.Export.All','AuditLog.Read.All'
+}
+#Gather Microsoft Accounts
+$Userproperties = @(
+    'AccountEnabled',
+    'DisplayName',
+    'mail',
+    'SignInActivity',
+    'UserPrincipalName')      
+$InactiveMicrosoftAccounts = Get-MgUser -Filter 'accountEnabled eq false' -Property $Userproperties -All:$true
+foreach ($MGAccount in $InactiveMicrosoftAccounts)
+{
+    #Each User Properties
+    $AccountEnabled = $MGAccount.AccountEnabled
+    $AssignedLicenses = Get-MgUserLicenseDetail -UserId $MGAccount.Id
+    $DisplayName = $MGAccount.DisplayName
+    $SignInActivity = $MGAccount.SignInActivity.LastSignInDateTime
+    $UserPrincipalName = $MGAccount.UserPrincipalName
+
+    #Create the Table
+    $Table = [PSCustomObject]@{
+        DisplayName      = $DisplayName
+        UserPrincipalName = $UserPrincipalName
+        AccountEnabled = $AccountEnabled
+        SignInActivity = $SignInActivity 
+        Licenses = $AssignedLicenses.SkuPartNumber -join ', '
+    }
+    $Table
+}
