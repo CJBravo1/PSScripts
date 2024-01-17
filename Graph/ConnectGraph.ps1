e13d697bf63738e5786b069ba0159604cc05cdea
Chris Jorenby <156940640+cjorenbyArcticWolf@users.noreply.github.com>
2024-01-17 15:22:24 -0600

    Additional Scopes Added

--- Graph/ConnectGraph.ps1
+++ Graph/ConnectGraph.ps1
@@ -18,11 +18,11 @@ if ($null -eq (Get-InstalledModule microsoft.graph -ErrorAction SilentlyContinue
         {
             "Write"
             {
-                Connect-MgGraph -Scopes 'Directory.ReadWrite.All','User.ReadWrite.All','Group.ReadWrite.All','User.Export.All','User.Read.All','Directory.AccessAsUser.All','User.ReadBasic.All','Directory.Read.All'
+                Connect-MgGraph -Scopes 'Directory.ReadWrite.All','User.ReadWrite.All','Group.ReadWrite.All','User.Export.All','User.Read.All','Directory.AccessAsUser.All','User.ReadBasic.All','Directory.Read.All','AuditLog.Read.All'
             }
             "Read"
             {
-                Connect-MgGraph -Scopes 'User.Read.All', 'Directory.AccessAsUser.All', 'User.ReadBasic.All', 'User.ReadWrite.All', 'Directory.Read.All', 'Directory.ReadWrite.All', 'Group.Read.All', 'User.Export.All'
+                Connect-MgGraph -Scopes 'User.Read.All', 'Directory.AccessAsUser.All', 'User.ReadBasic.All', 'User.ReadWrite.All', 'Directory.Read.All', 'Directory.ReadWrite.All', 'Group.Read.All', 'User.Export.All','AuditLog.Read.All'
             }
             
         }
