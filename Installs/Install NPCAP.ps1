e44be4810294b9c3fdead75836e4ea4d5c51dffe
Chris Jorenby <156940640+cjorenbyArcticWolf@users.noreply.github.com>
2024-02-28 12:57:07 -0600

    Repository Cleanup

--- Installs/Install NPCAP.ps1
+++
@@ -1,7 +0,0 @@
-#Install NPCAP
-iwr -uri https://nmap.org/npcap/dist/npcap-0.86.exe -OutFile npcap-0.86.exe
-./npcap-0.86.exe
-
-#Install Wireshark
-iwr -uri https://www.wireshark.org/download/win64/all-versions/Wireshark-win64-4.0.7.msi -OutFile .\Wireshark-win64-4.0.7.msi
-.\Wireshark-win64-4.0.7.msi /qb
--- VMWare/PSModules/CopyVMWareModules.ps1
+++
@@ -1,3 +0,0 @@
-$ModulePath = $env:PSModulePath
-$ModulePath.Split(";") | foreach {copy .\VMWare\ $_ -Recurse -Verbose}
-$ModulePath.Split(";") | foreach {copy .\VMware.Hv.Helper\ $_ -Recurse -Verbose}
